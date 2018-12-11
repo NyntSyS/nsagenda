5c261a970d717080ca93b0088e4bb7579b5f98e7
samuel <eldejimo3@gmail.com>
2018-12-11 17:21:10 +0100

    prueba

--- app/src/main/java/com/dam/nestor_samuel/nsagenda/ActivityLogo.java
+++ app/src/main/java/com/dam/nestor_samuel/nsagenda/ActivityLogo.java
@@ -9,7 +9,7 @@ public class ActivityLogo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
-
+        //hola
         
     }
 }
